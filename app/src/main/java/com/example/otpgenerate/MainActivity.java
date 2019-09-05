990dfd073dcba381ff719782a45f32b49773d812
bijon95 <bijoninfo@gmail.com>
2019-09-05 22:14:00 +0600

    Initial commit

---
+++ app/src/main/java/com/example/otpgenerate/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.otpgenerate;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+//To do//
+                            return;
+                        }
+
+// Get the Instance ID token//
+                        String token = task.getResult().getToken();
+                        Toast.makeText(getApplicationContext(),token,Toast.LENGTH_SHORT).show();
+                       // String msg = getString(R.string.fcm_token, token);
+                       // Log.d(TAG, msg);
+
+                    }
+                });
+
+    }
+}
